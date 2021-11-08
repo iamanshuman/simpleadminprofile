31173528f1b0921c1a50c508bf7b942b294b47a3
Anshuman <Anshuman.Shekhar2002@gmail.com>
2021-11-08 23:49:37 +0530

    new github and linkedin icon has been added which will direct users to there respective application account on clicking them

--- app/src/main/java/fragments/PageFragment1.java
+++ app/src/main/java/fragments/PageFragment1.java
@@ -1,10 +1,14 @@
 package fragments;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.example.admin.R;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,14 +16,42 @@ import androidx.fragment.app.Fragment;
 
 public class PageFragment1 extends Fragment {
 
+    private ImageView img1,img2,img;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @NonNull ViewGroup container,
                              @NonNull Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_admin1, container, false);
+
+        img1 = rootView.findViewById(R.id.github);
+        img2 = rootView.findViewById(R.id.linkedin);
 
 
-        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.fragment_admin1,container,false);
-        return rootview;
+
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open("https://github.com/iamanshuman");
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open("https://www.linkedin.com/in/anshuman-shekhar-0735231a6/");
+            }
+        });
+
+        return rootView;
+    }
+
+    private void open(String s) {
+        Uri uri = Uri.parse(s);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
     }
 }
--- app/src/main/java/fragments/PageFragment3.java
+++ app/src/main/java/fragments/PageFragment3.java
@@ -1,16 +1,21 @@
 package fragments;
 
+import android.content.Intent;
 import android.example.admin.R;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PageFragment3 extends Fragment {
+
+    private ImageView img1, img2;
 
     @Nullable
     @Override
@@ -19,7 +24,32 @@ public class PageFragment3 extends Fragment {
                              @NonNull Bundle savedInstanceState) {
 
 
-        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.fragment_admin3,container,false);
-        return rootview;
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_admin3, container, false);
+
+        img1 = rootView.findViewById(R.id.github);
+        img2 = rootView.findViewById(R.id.linkedin);
+
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open("https://github.com/iamanshuman");
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open("https://www.linkedin.com/in/anshuman-shekhar-0735231a6/");
+            }
+        });
+
+        return rootView;
+    }
+
+    private void open(String s) {
+        Uri uri = Uri.parse(s);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
     }
 }
